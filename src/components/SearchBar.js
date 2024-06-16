cf2f6f8cc6932cfe4080a66aec570a895a9ab0cc
pranaydo <pranaydodake7@gmail.com>
2024-06-16 22:21:15 +0530

    completed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -24,33 +24,104 @@ const SearchBar = () => {
     setNewTodo((values) => ({ ...values, [name]: value }));
   };
   console.log("allTOdo", allTodo);
-
+  
+  const onDelete = (index) => {
+    console.log("delete", index);
+    const newArray = allTodo?.filter((todo,ind) => ind !== index);
+    console.log(newArray);
+    setallTodo(newArray);
+  };
   return (
-<>    <div style={{background:'grey' , textAlign:'center' , padding:'20px', margin:'10px'}}>
-      <form onSubmit={handleNewTodo}>
-        <div>
-        <label style={{color:'white'}}>Todo Name : </label>
+    <>
+      {" "}
+      <div
+        style={{
+          background: "grey",
+          textAlign: "center",
+          padding: "20px",
+          margin: "10px",
+        }}
+      >
+        <form onSubmit={handleNewTodo}>
+          <div>
+            <label style={{ color: "white" }}>Todo Name : </label>
 
-        <input
-          name="newTodoName"
-          onChange={handleOnChange}
-          value={newTodo?.newTodoName}
-        />
-        </div>
-        <div>
-        <label style={{color:'white'}}>Description : </label>
-        <input
-          name="newTodoDescription"
-          onChange={handleOnChange}
-          value={newTodo?.newTodoDescription}
-        />
-        </div>
+            <input
+              name="newTodoName"
+              onChange={handleOnChange}
+              value={newTodo?.newTodoName}
+            />
+          </div>
+          <div>
+            <label style={{ color: "white" }}>Description : </label>
+            <input
+              name="newTodoDescription"
+              onChange={handleOnChange}
+              value={newTodo?.newTodoDescription}
+            />
+          </div>
 
-        <button style={{marginTop:'10px', padding:"5px", border:'1px solid black', borderRadius:'25px'}} type="submit"> Create</button>
-      </form>
-    </div>
-      <TodoList allTodo={allTodo} />
-</>
+          <button
+            style={{
+              marginTop: "10px",
+              padding: "5px",
+              border: "1px solid black",
+              borderRadius: "25px",
+            }}
+            type="submit"
+          >
+            {" "}
+            Create
+          </button>
+        </form>
+      </div>
+      {allTodo?.length > 0 ? (
+        //   <TodoList allTodo={allTodo} setallTodo={setallTodo} /> :
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            background: "grey",
+            padding: "10px",
+          }}
+        >
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Sr.No.</th>
+                  <th>Todo Name</th>
+                  <th>Description</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allTodo?.map((item, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{item?.newTodoName}</td>
+                    <td>{item?.newTodoDescription}</td>
+                    <td>
+                      {
+                        <button
+                          onClick={() => {
+                            onDelete(index);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      }
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) : (
+        <div style={{ textAlign: "center" }}> No Todo Available </div>
+      )}
+    </>
   );
 };
 
